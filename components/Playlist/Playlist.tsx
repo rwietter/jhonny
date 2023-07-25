f041bba819585dc9c5c3db0ce0173c4bbff65b64
rwietter <rwietter@zohomail.com>
2023-07-25 20:29:38 -0300

    fix(web): refactor fetch to useSWR

--- components/Playlist/Playlist.tsx
+++ components/Playlist/Playlist.tsx
@@ -1,36 +1,48 @@
 import { IPlaylist } from "@types";
 import Track from "./components/Track";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string): Promise<IPlaylist[]> =>
+  fetch(url).then((r) => r.json());
 
 const Playlist = () => {
-  const [tracks, setTracks] = useState<IPlaylist[]>([]);
+  const { data, error } = useSWR("/api/songs", fetcher);
 
-  const fetchMusics = async () => {
-    const data = await fetch("/api/songs");
-    return await data.json();
-  };
+  if (typeof data === "undefined") return null;
 
-  useEffect(() => {
-    fetchMusics().then((data) => {
-      setTracks(data);
-    });
-  }, []);
-
+  if (error)
+    return (
+      <p className="text-secondary bg-primary text-center p-8">
+        Ocorreu um erro ao carregar Músicas :(
+      </p>
+    );
 
   return (
-    <section className='h-auto lg:h-[40rem] relative w-full bg-primary py-6 pb-32 lg:pb-6 px-3 md:px-8 lg:px-16'>
-      <h1 className="text-center text-6xl xl:text-7xl text-secondary font-bold font-primary">Músicas</h1>
-      <div className="w-full pt-10 flex flex-col items-center ">
-        {tracks.map((track, index) => (
-          <Track
-            key={index}
-            name={track.name}
-            url={track.url}
-            year={track.year} />
-        ))}
-      </div>
+    <section className="h-auto lg:h-[40rem] relative w-full bg-primary py-6 pb-32 lg:pb-6 px-3 md:px-8 lg:px-16">
+      <h1 className="text-center text-6xl xl:text-7xl text-secondary font-bold font-primary">
+        Músicas
+      </h1>
+      <Suspense
+        fallback={
+          <p className="text-secondary bg-primary text-center p-8">
+            Loading...
+          </p>
+        }
+      >
+        <div className="w-full pt-10 flex flex-col items-center ">
+          {data.map((track, index) => (
+            <Track
+              key={index}
+              name={track.name}
+              url={track.url}
+              year={track.year}
+            />
+          ))}
+        </div>
+      </Suspense>
     </section>
-  )
-}
+  );
+};
 
 export { Playlist };
--- components/Shows/shows.tsx
+++ components/Shows/shows.tsx
@@ -1,34 +1,46 @@
-import { ShowCard } from './components';
-import { IShow } from '@types'
-import { useEffect, useState } from 'react';
+import { ShowCard } from "./components";
+import { IShow } from "@types";
+import { Suspense } from "react";
+import useSWR from "swr";
 
+const fetcher = (url: string): Promise<IShow[]> =>
+  fetch(url).then((r) => r.json());
 
 export const Shows = () => {
-  const [shows, setShows] = useState<IShow[]>([]);
+  const { data, error } = useSWR("/api/performance", fetcher);
 
- const fetchShows = async () => {
-   const data = await fetch("/api/performance");
-   return await data.json();
- };
+  if (typeof data === "undefined") return null;
 
-  useEffect(() => {
-    fetchShows().then((data) => {
-      setShows(data);
-    });
-  }, []);
+  if (error || typeof data === "undefined")
+    return (
+      <p className="text-secondary bg-primary text-center p-8">
+        Ocorreu um erro ao carregar Músicas :(
+      </p>
+    );
 
   return (
-    <section className='h-auto lg:h-auto relative w-full bg-primary py-6 pb-24 lg:pb-36 px-3 md:px-8 lg:px-16'>
-      <h1 className="text-center text-6xl xl:text-7xl text-secondary font-bold font-primary">Shows</h1>
-      <div className="w-full pt-10 flex flex-col items-center">
-        {shows.map((show, index) => (
-          <ShowCard
-            key={index}
-            show={show.show}
-            local={show.local}
-            date={show.date} />
-        ))}
-      </div>
+    <section className="h-auto lg:h-auto relative w-full bg-primary py-6 pb-24 lg:pb-36 px-3 md:px-8 lg:px-16">
+      <h1 className="text-center text-6xl xl:text-7xl text-secondary font-bold font-primary">
+        Shows
+      </h1>
+      <Suspense
+        fallback={
+          <p className="text-secondary bg-primary text-center p-8">
+            Loading...
+          </p>
+        }
+      >
+        <div className="w-full pt-10 flex flex-col items-center">
+          {data.map((show, index) => (
+            <ShowCard
+              key={index}
+              show={show.show}
+              local={show.local}
+              date={show.date}
+            />
+          ))}
+        </div>
+      </Suspense>
     </section>
-  )
-}
+  );
+};
